8bdd3bb97b8164e0d9fd14cfade185dd48f6fab2
Rohollah <Rohollah903@gmail.com>
2024-09-25 10:31:37 +0330

    Adding length_estimator

--- dataset/prepare/download_models.sh
+++ dataset/prepare/download_models.sh
@@ -2,6 +2,8 @@ echo -e "Downloading pretrained models"
 
 mkdir -p ./output/vq/
 mkdir -p ./output/t2m/
+mkdir -p ./checkpoints/t2m/length_estimator/model/
+
 cd ./output
 
 gdown --fuzzy https://drive.google.com/file/d/1fchcM7vWJpMKbDP7wTVrufIgfyYaXK49/view?usp=sharing
@@ -12,5 +14,9 @@ gdown --fuzzy https://drive.google.com/file/d/1ZldeaE9mYOAsG9B2UM-Oc_BpQw75xW4l/
 mv trans_best_fid.pth t2m/
 
 cd ..
+
+cd ./checkpoints/t2m/length_estimator/model/
+gdown --fuzzy https://drive.google.com/file/d/1eFphHaWX669pXVgXJTRvCOdKonQ0Pns_/view?usp=sharing
+
 
 echo -e "Finished"
